f235a9d0c5239428c6c34a723b06b1a690ae520f
Steve Marshall <steve@stevemarshall.com>
2016-01-07 13:23:27 +0000

    Allow query caching to be disabled for routes
    This can be used by setting `disable_query_cache: true` on a route:
    
        # config/routes.rb
        get 'healthcheck', to: 'healthcheck#index', disable_query_cache: true
    
    Or using a scope to disable query caching for multiple routes:
    
        scope disable_query_cache: true do
          # routes
        end

--- lib/optional_query_cache.rb
+++ lib/optional_query_cache.rb
@@ -4,4 +4,14 @@ require 'optional_query_cache/version'
 
 # Rack middleware to allow per-route disabling of ActiveRecord::QueryCache
 class OptionalQueryCache < ActiveRecord::QueryCache
+  def call(env)
+    path_parameters = env['action_dispatch.routes'].recognize_path(
+      env['REQUEST_PATH']
+    )
+    if path_parameters[:disable_query_cache]
+      @app.call(env)
+    else
+      super(env)
+    end
+  end
 end
--- spec/optional_query_cache_spec.rb
+++ spec/optional_query_cache_spec.rb
@@ -13,10 +13,21 @@ RSpec.describe OptionalQueryCache do
   end
 
   let :env do
-    Rack::MockRequest.env_for('http://example.org')
+    double.tap do |env|
+      allow(env).to receive(:[]).with('REQUEST_PATH').and_return('')
+    end
   end
 
   context 'with a normal route' do
+    before do
+      allow(env).to receive(:[]).with('action_dispatch.routes').and_return(
+        double(
+          'routes',
+          recognize_path: {}
+        )
+      )
+    end
+
     it 'permits query caching' do
       expect(ActiveRecord::Base).to receive(:connection).and_return(
         double(
@@ -25,6 +36,24 @@ RSpec.describe OptionalQueryCache do
           enable_query_cache!: nil
         )
       )
+      subject.call(env)
+    end
+  end
+
+  context 'with disable_query_cache set on the route' do
+    before do
+      allow(env).to receive(:[]).with('action_dispatch.routes').and_return(
+        double(
+          'routes',
+          recognize_path: {
+            disable_query_cache: true
+          }
+        )
+      )
+    end
+
+    it 'disables query caching for the route' do
+      expect(ActiveRecord::Base).not_to receive(:connection)
       subject.call(env)
     end
   end
